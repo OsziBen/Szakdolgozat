6776ffe62476ea819dc417dcc76602e393cdfeeb
OsziBen <osziben2@gmail.com>
2025-04-23 23:37:00 +0200

    Add NpgsqlConnectionStringBuilder

--- BACKEND/BackgammonApp/Program.cs
+++ BACKEND/BackgammonApp/Program.cs
@@ -1,3 +1,7 @@
+using BackgammonApp.Data;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var baseConnectionString = builder.Configuration.GetConnectionString("ApplicationDbContext");
+var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+var connectionStringBuilder = new NpgsqlConnectionStringBuilder(baseConnectionString)
+{
+    Password = dbPassword
+};
+
+var fullConnectionString = connectionStringBuilder.ConnectionString;
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(fullConnectionString));
 
 var app = builder.Build();
 
