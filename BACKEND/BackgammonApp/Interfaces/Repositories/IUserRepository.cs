5c0d07970d55c035e3441304ed577e0999754573
OsziBen <osziben2@gmail.com>
2025-04-28 23:11:57 +0200

    Create and organize interface

---
+++ BACKEND/BackgammonApp/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,22 @@
+﻿using BackgammonApp.Data;
+
+namespace BackgammonApp.Interfaces.Repositories
+{
+    public interface IUserRepository
+    {
+        Task CreateAsync(User user);
+
+        Task<bool> DeleteAsync(Guid id);
+
+        Task<bool> EmailExistsAsync(string email);
+
+        Task<List<User>> GetAllAsync();
+
+        Task<User?> GetByEmailAsync(string email);
+
+        Task<User?> GetByIdAsync(Guid id);
+
+        Task<int> SaveChangesAsyc();
+
+    }
+}
