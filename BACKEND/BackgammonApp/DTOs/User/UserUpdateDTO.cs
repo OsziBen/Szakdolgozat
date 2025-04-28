62438572d7f28356fbb7caf8c3921314229274ed
OsziBen <osziben2@gmail.com>
2025-04-28 23:12:18 +0200

    Create and organize DTOs

---
+++ BACKEND/BackgammonApp/DTOs/User/UserUpdateDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackgammonApp.DTOs.User
+{
+    public class UserUpdateDTO
+    {
+        [Required]
+        [StringLength(50)]
+        public required string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public required string LastName { get; set; }
+
+        [Required]
+        [Range(0, 150, ErrorMessage = "Age must be between 0 and 150")]
+        public required int Age { get; set; }
+    }
+}
