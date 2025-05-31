4d2a734e07d130966945db3aa3cf52483db5090b
OsziBen <osziben2@gmail.com>
2025-06-01 00:55:54 +0200

    Create user model for auth and future role functionalities

---
+++ BACKEND/BackgammonApp/Models/User/AppUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BackgammonApp.Models.User
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public DateOnly DateOfBirth { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        // database-relations
+
+    }
+}
