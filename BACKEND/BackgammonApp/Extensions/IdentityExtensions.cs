fa9efe962ab640421068c5faa7eee8fd0c858815
OsziBen <osziben2@gmail.com>
2025-06-01 00:52:35 +0200

    Create extension methods for Identity extension functionalities

---
+++ BACKEND/BackgammonApp/Extensions/IdentityExtensions.cs
@@ -0,0 +1,81 @@
+﻿using BackgammonApp.Data;
+using BackgammonApp.Models.User;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace BackgammonApp.Extensions
+{
+    public static class IdentityExtensions
+    {
+        public static IServiceCollection AddIdentityHandlersAndStores(this IServiceCollection services)
+        {
+            services.AddIdentityApiEndpoints<AppUser>()
+                    .AddRoles<IdentityRole>()
+                    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigureIdentityOptions(this IServiceCollection services)
+        {
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+                // Password options
+                //options.Password.RequiredLength = 8;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+
+            });
+
+            return services;
+        }
+
+        // Auth = Authentication + Authorization
+        public static IServiceCollection AddIdentityAuth(
+            this IServiceCollection services,
+            IConfiguration config)
+        {
+            services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.SaveToken = false;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(
+                            config["AppSettings:JWTSecret"]!)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                };
+            });
+
+            services.AddAuthorization(options =>
+            {
+                options.FallbackPolicy = new AuthorizationPolicyBuilder()
+                       .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                       .RequireAuthenticatedUser()
+                       .Build();
+
+                // Policies
+                //options.AddPolicy
+            });
+
+            return services;
+        }
+
+        public static WebApplication AddIdentityAuthMiddlewares(this WebApplication app)
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            return app;
+        }
+    }
+}
