dd56643029a396d8a796f532c75c3d80c2802437
OsziBen <osziben2@gmail.com>
2025-06-01 00:49:24 +0200

    Add temporary endpoints for auth functionalities

---
+++ BACKEND/BackgammonApp/Endpoints(temp)/AccountEndpoints.cs
@@ -0,0 +1,34 @@
+﻿using BackgammonApp.Models.User;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace BackgammonApp.Endpoints_temp_
+{
+    public static class AccountEndpoints
+    {
+        public static IEndpointRouteBuilder MapAccountEndpoints(this IEndpointRouteBuilder app)
+        {
+            app.MapGet("/UserProfile", GetUserProfile);
+
+            return app;
+        }
+
+        [Authorize]
+        private static async Task<IResult> GetUserProfile(ClaimsPrincipal user,
+            UserManager<AppUser> userManager)
+        {
+            string userID = user.Claims.First(x => x.Type == "UserID").Value;
+            var userDetails = await userManager.FindByIdAsync(userID);
+
+
+            return Results.Ok(
+                new
+                {
+                    Email = userDetails?.Email,
+                    FirstName = userDetails?.FirstName,
+                    LastName = userDetails?.LastName
+                });
+        }
+    }
+}
---
+++ BACKEND/BackgammonApp/Endpoints(temp)/AuthorizationEndpoints.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace BackgammonApp.Endpoints_temp_
+{
+    public static class AuthorizationEndpoints
+    {
+        public static IEndpointRouteBuilder MapAuthorizationEndpoints(this IEndpointRouteBuilder app)
+        {
+            app.MapGet("/AdminOnly", AdminOnly);
+
+            return app;
+        }
+
+        [Authorize(Roles = "Admin")]
+        private static string AdminOnly()
+        {
+            return "Admin only";
+        }
+    }
+}
