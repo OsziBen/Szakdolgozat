ac890a24d1a3bfe5a57fef8582c400b70a637678
OsziBen <osziben2@gmail.com>
2025-06-07 01:43:22 +0200

    Configure CORS frontend connection

--- BACKEND/BackgammonApp/Extensions/AppConfigExtensions.cs
+++ BACKEND/BackgammonApp/Extensions/AppConfigExtensions.cs
@@ -4,11 +4,14 @@ namespace BackgammonApp.Extensions
 {
     public static class AppConfigExtensions
     {
-        public static WebApplication ConfigCORS(
-    this WebApplication app,
-    IConfiguration config)
+        public static WebApplication ConfigureCORS(
+            this WebApplication app,
+            IConfiguration config)
         {
-            app.UseCors();
+            app.UseCors(options =>
+                options.WithOrigins("http://localhost:4200")
+                       .AllowAnyMethod()
+                       .AllowAnyHeader());
 
             return app;
         }
