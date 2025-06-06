593fae7581fb703a22d150e999e19555fd13a138
OsziBen <osziben2@gmail.com>
2025-06-07 01:50:20 +0200

    Add authentication service for backend configuration

---
+++ FRONTEND/BackgammonAppClient/src/app/shared/services/auth.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+  baseURL = 'http://localhost:5125/api';
+
+  createUser(formData: any){
+    formData.role = "Admin";  // TEMP!
+
+    return this.http.post(this.baseURL + '/signup', formData)
+  }
+}
