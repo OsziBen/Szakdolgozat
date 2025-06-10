635b2a506272030d393fe6bb9533ea5e3e06e109
OsziBen <osziben2@gmail.com>
2025-06-10 11:32:28 +0200

    Add main layout, admin-only and forbidden page

--- FRONTEND/BackgammonAppClient/src/app/app.routes.ts
+++ FRONTEND/BackgammonAppClient/src/app/app.routes.ts
@@ -3,6 +3,11 @@ import { UserComponent } from './user/user.component';
 import { RegistrationComponent } from './user/registration/registration.component';
 import { LoginComponent } from './user/login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { authGuard } from './shared/auth.guard';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { AdminOnlyComponent } from './access/admin/admin-only/admin-only.component';
+import { ForbiddenComponent } from './forbidden/forbidden.component';
+import { claimReq } from './shared/utils/claimReq-utils';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/signin', pathMatch:'full'},
@@ -15,5 +20,19 @@ export const routes: Routes = [
             }
         ]
     },
-    {path: 'dashboard', component: DashboardComponent}
+    { path: '',
+        component: MainLayoutComponent,
+        canActivate: [authGuard],
+        canActivateChild: [authGuard],
+        children: [
+            { path: 'dashboard', component: DashboardComponent
+            },
+            { path: 'admin-only',
+                component: AdminOnlyComponent,
+                data: {claimReq: claimReq.adminOnly}
+            },
+            { path: 'forbidden', component: ForbiddenComponent
+            }
+        ]
+    }
 ];
