badc18c4b9bf1aa70cd87b96b45cd3142e783b35
OsziBen <osziben2@gmail.com>
2025-06-21 22:30:08 +0200

    Add service interface for token generation methods

---
+++ BACKEND/BackgammonApp/Interfaces/Services/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using BackgammonApp.Models.User;
+
+namespace BackgammonApp.Interfaces.Services
+{
+    public interface ITokenService
+    {
+        string CreateToken(AppUser user, IList<string> roles);
+    }
+}
