9ad9f88af4a8d9edb152122f8fd227b3532041c3
OsziBen <osziben2@gmail.com>
2025-06-21 21:26:20 +0200

    Add controller class for user authentication

---
+++ BACKEND/BackgammonApp/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using BackgammonApp.DTOs.AppUser;
+using BackgammonApp.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackgammonApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("signin")]
+        [AllowAnonymous]
+        public async Task<ActionResult> SignIn([FromBody] LoginDTO dto)
+        {
+            var result = await _authService.SignInAsync(dto);
+
+            if (result == null)
+            {
+                return BadRequest(new {message = "Username or Password is incorrect."});
+            }
+
+            return Ok(result);
+        }
+    }
+}
