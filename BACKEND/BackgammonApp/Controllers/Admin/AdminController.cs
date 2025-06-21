6478c1f1a35b95745e4415d1ce5fa9a888099903
OsziBen <osziben2@gmail.com>
2025-06-21 21:24:47 +0200

    Add controller class for admin only methods

---
+++ BACKEND/BackgammonApp/Controllers/Admin/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using BackgammonApp.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackgammonApp.Controllers.Admin
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly IAdminService _adminService;
+
+        public AdminController(IAdminService adminService)
+        {
+            _adminService = adminService;
+        }
+
+        [HttpGet("AdminOnly")]
+        [Authorize(Roles = "Admin")]
+        public IActionResult AdminOnly()
+        {
+            var result = _adminService.GetAdminMessage();
+
+            return Ok(result);
+        }
+    }
+}
