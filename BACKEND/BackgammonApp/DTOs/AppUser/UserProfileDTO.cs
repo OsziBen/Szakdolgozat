f58bec7b17e8c8d2b0308d1d38fb42f4974d8144
OsziBen <osziben2@gmail.com>
2025-06-21 21:20:51 +0200

    Create UserProfile DTO for accessing user data

---
+++ BACKEND/BackgammonApp/DTOs/AppUser/UserProfileDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace BackgammonApp.DTOs.AppUser
+{
+    public class UserProfileDTO
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateOnly DateOfBirth { get; set; }
+    }
+}
