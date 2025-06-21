26658f64c173a32538b95f569a72037c57acc6d7
OsziBen <osziben2@gmail.com>
2025-06-21 21:16:01 +0200

    Create registration DTO for creating new user profile

---
+++ BACKEND/BackgammonApp/DTOs/AppUser/UserRegistrationDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace BackgammonApp.DTOs.AppUser
+{
+    public class UserRegistrationDTO
+    {
+        public required string Email { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string DateOfBirth { get; set; }
+        public required string Password { get; set; }
+    }
+}
