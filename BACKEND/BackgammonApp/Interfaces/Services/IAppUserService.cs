6cf0600ee7a4cbfde328ed876f8d0672bf12ac0d
OsziBen <osziben2@gmail.com>
2025-06-21 22:29:14 +0200

    Add service interface for user methods

---
+++ BACKEND/BackgammonApp/Interfaces/Services/IAppUserService.cs
@@ -0,0 +1,10 @@
+﻿using BackgammonApp.DTOs.AppUser;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackgammonApp.Interfaces.Services
+{
+    public interface IAppUserService
+    {
+        public Task<UserProfileDTO?> GetAppUserProfileAsync();
+    }
+}
